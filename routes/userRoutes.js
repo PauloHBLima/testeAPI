2f8365afe5107efbb7f63f24e68b87250ba15cfc
Paulo Henrique <ph.blima30@gmail.com>
2022-10-11 00:17:03 -0300

    Rota User

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const verifyToken = require('../middlewares/verifyToken');
 
 router.post("/register", authController.create)  //criação de dados
 router.post("/login", authController.login)  //login opcional
-router.get("/session_id", authController.viewSessionId)  // dados de todas as session_id
+router.get("/users", authController.viewSessionId)  // dados de todas os users
 router.get("/user/:id", verifyToken, authController.viewOneUser)   // dados de um unico user
 router.delete("/user/:id", authController.delete)  // deletar o user
 
